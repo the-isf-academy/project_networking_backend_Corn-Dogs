a9d58956e33ccf021b0a6394b46678d56cb5e9eb
nicholas <0014212@student.isf.edu.hk>
2024-10-15 15:29:04 +0800

    #today wrote more models such as /all and /new. then i tested it in httpie and in shell. turns out they both don't work so that's good that I can test them well

--- app/models.py
+++ app/models.py
@@ -5,5 +5,17 @@ from banjo.models import Model, StringField, IntegerField, FloatField, BooleanFi
 
 class Rather(Model):
     noun = StringField()
-    adjective = StringField()
+    noun2 = StringField()
     verb = StringField()
+    timechosen = IntegerField()
+    happy = BooleanField()
+
+
+    def json_response(self):
+        
+        return {
+            'id': self.id,
+            'scenario': self.noun + + self.verb + self.noun2,
+            'times chosen': self.timechosen,
+            'positive': self.happy
+        }
--- app/views.py
+++ app/views.py
@@ -1,5 +1,30 @@
 # views.py
-
 from banjo.urls import route_get, route_post
+from .models import Rather
 from settings import BASE_URL
 
+
+
+@route_post(BASE_URL + 'new', args={'noun':str, 'verb':str, 'noun2':str, 'happy': bool})
+def new_scenario(args):
+    new_scenario = Rather(
+        noun = args['noun'],
+        verb = args['verb'],
+        noun2 = args['noun2'],
+        happy = args['happy'],
+        timechosen = 0
+    )
+
+    new_scenario.save()
+
+    return {'fortune': new_scenario.json_response()}
+
+
+@route_get(BASE_URL + 'all')
+def all_scenarios(args):
+    scenario_list = []
+
+    for scenario in Rather.objects.all():
+        scenario_list.append(scenario.json_response())
+
+    return {'fortunes':scenario_list}
