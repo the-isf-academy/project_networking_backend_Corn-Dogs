9b4526b217d472d2bd4138e8cd39eb573d71e888
nicholas <0014212@student.isf.edu.hk>
2024-10-23 09:09:10 +0800

    #today i worked on option and delete. these are very useful for playing the game and managing proper gameplay. delete is used for if people submit multiple

--- app/models.py
+++ app/models.py
@@ -3,11 +3,11 @@
 from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
 
 
-class Rather(Model):
+class WouldYouRather(Model):
     noun = StringField()
     noun2 = StringField()
     verb = StringField()
-    timechosen = IntegerField()
+    timeschosen = IntegerField()
     happy = BooleanField()
 
 
@@ -15,27 +15,27 @@ class Rather(Model):
         return {
             'id': self.id,
             'scenario': self.noun + " " + self.verb + " " + self.noun2,
-            'times chosen': self.timechosen,
+            'times chosen': self.timeschosen,
             'positive': self.happy
         }
     
 
     def change_verb(self, new_word):
         self.verb = new_word
-        self.timechosen = 0
-        self.save
+        self.timeschosen = 0
+        self.save()
           
     def change_noun(self, new_word):
         self.noun = new_word
-        self.timechosen = 0
-        self.save
+        self.timeschosen = 0
+        self.save()
  
     def change_noun2(self, new_word):
         self.noun2 = new_word
-        self.timechosen = 0
-        self.save
+        self.timeschosen = 0
+        self.save()
 
-def json_options(self):
+def json_options(self, id1, id2):
         
         return {
             'id': self.id,
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 # views.py
 
 from banjo.urls import route_get, route_post
-from .models import Rather
+from .models import WouldYouRather
 from settings import BASE_URL
 from random import *
 from num2words import num2words         #pip install num2words in terminal
@@ -9,78 +9,89 @@ from num2words import num2words         #pip install num2words in terminal
 
 @route_post(BASE_URL + 'new', args={'noun':str, 'verb':str, 'noun2':str, 'happy': bool})
 def new_scenario(args):
-    new_scenario = Rather(
+    new_scenario = WouldYouRather(
         noun = args['noun'],
         verb = args['verb'],
         noun2 = args['noun2'],
         happy = args['happy'],
-        timechosen = 0
+        timeschosen = 0
     )
 
     new_scenario.save()
 
-    return {'riddle': new_scenario.json_response()}
+    return {'scenario': new_scenario.json_response()}
 
 
 @route_get(BASE_URL + 'all')
 def all_scenarios(args):
     scenario_list = []
 
-    for scenario in Rather.objects.all():
+    for scenario in WouldYouRather.objects.all():
         scenario_list.append(scenario.json_response())
 
     return {'riddles':scenario_list}
+
+@route_get(BASE_URL + 'one', args={'id':int})
+def all_scenarios(args):
+    if WouldYouRather.objects.filter(id=args['id']).exists():
+        one_scenario = WouldYouRather.objects.get(id=args['id'])
+        return {'scenario': one_scenario.json_response()}
+    else:
+        return {'error':'no fortune exists'}
 
 
 
 @route_post(BASE_URL +'change', args={'id':int, 'wordclass':str, 'new_word':str})
 def change_scenario(args):
-    if Rather.objects.filter(id=args['id']).exists():
+    if WouldYouRather.objects.filter(id=args['id']).exists():
 
-        one_rather = Rather.objects.get(id=args['id'])
+        one_scenario = WouldYouRather.objects.get(id=args['id'])
 
         if args['wordclass'] == 'noun':
-            one_rather.change_noun(args['new_word'])
+            one_scenario.change_noun(args['new_word'])
 
         elif args['wordclass'] == 'verb':
-            one_rather.change_verb(args['new_word'])
+            one_scenario.change_verb(args['new_word'])
 
         elif args['wordclass'] == 'noun2':
-            one_rather.change_noun2(args['new_word'])
+            one_scenario.change_noun2(args['new_word'])
 
         else:
             return {"changing scenario":"no word class exists. please enter either 'noun', 'verb', or 'noun2'."}
     
-        return {'scenario': one_rather.json_response()}
+        return {'scenario': one_scenario.json_response()}
+    
     else:
-        return {'erorr':'no fortune exists'}
+        return {'error':'no fortune exists'}
     
 
 @route_get(BASE_URL +'option')    ##where the user gets given two random scenarios/options
 def choose_scenario(args):
     good_list = []
     bad_list = []
+    lists = [good_list, bad_list]
     scenario_matches =[] 
 
-    for scenario in Rather.objects.all():
+    for scenario in WouldYouRather.objects.all():
         if scenario.happy == True:       #sorts them into good and bad (in order to be fair)
             good_list.append(scenario.json_response())
         else:
             bad_list.append(scenario.json_response)
 
-    mood = random.choice(good_list, bad_list)   #chooses randomly between good or bad
+    mood = random.choice(lists)   #chooses randomly between good or bad
 
     scenario1 = random.choice(mood)(id=args['id'])        #these four lines chooses two random scenarios with the same mood 
     scenario1 = scenario1.id                              #and stores only their id
     scenario2 = random.choice(mood)
     scenario2 = scenario2.id
 
-    for i in range():  
-        kevin = []                                                       #defns it as a list         
-        kevin.append(scenario1)                                          #adds scen. 1 and 2 to the list
-        kevin.append(scenario2)
-        kevin = num2words(len(scenario_matches)+1) 
-        scenario_matches.append(num2words(len(scenario_matches)+1) )       #adds them to matches
+
+    kevin = []                                                       #defns a list         
+    kevin.append(scenario1)                                          #adds scen. 1 and 2 to the list
+    kevin.append(scenario2)
+    kevin = num2words(len(scenario_matches)+1)                       #new name
+    scenario_matches.append(num2words(len(scenario_matches)+1) )       #adds them to matches
+    return(scenario_matches)
 
 
 
@@ -92,11 +103,23 @@ def choose_scenario(args):
     #adds the name to scneario_matches
 
 
-@route_post(BASE_URL +'choose', args={'id':int, 'wordclass':str, 'new_word':str})
-def choose_scenario(args):
+@route_post(BASE_URL + 'delete', args={'id':int,'password':str})
+def delete_scenario(args):
+    if WouldYouRather.objects.filter(id=args['id']).exists():
+        if args['password'] == 'Chromebook':
+            return {'deleting scenario': delete_scenario.json_response()}
+            WouldYouRather.objects.get(id=args['id']).delete()
+        else:
+            return {'error':'authrorization password incorect'}
+    else:
+        return {'error':'no fortune exists'}
+    
+
+#@route_post(BASE_URL +'choose', args={'id':int, 'wordclass':str, 'new_word':str})
+#def choose_scenario(args):
     
     #gets the most recent input as your matched scenarios
-    #
+
 
     #option - get two random options
     #delete
